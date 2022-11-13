6d3765e71c1bd4d3eeb763aa769cff7e0ca292fb
Dan Fritchman <dan@fritch.mn>
2022-11-13 05:36:27 +0000

    Working test.py. More README instructions

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ The (I think?) important things to run in a GitHub codespace:
 ```
 conda activate base
 conda install -y -c litex-hub open_pdks.sky130a
+
+git clone git@github.com:dan-fritchman/Hdl21.git
+cd Hdl21
+git checkout 8eef2239920e4bf61e3b378f028e985d1d41a106
+cd ..
+
+git clone git@github.com:Vlsir/Vlsir.git
+cd Vlsir
+git checkout 2079c44fd7b34023c737a7c01ea624b50289524f
+cd ..
+
 conda install -y poetry
 conda upgrade pip
 pip install -e ".[dev]"
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-from decimal import Decimal
 from pathlib import Path
 import os
 from typing import Tuple
 
 import hdl21 as h
+from hdl21.prefix import m, UNIT
 from hdl21.primitives import Vdc
 from vlsirtools.spice import SimOptions, SupportedSimulators, ResultFormat
 
@@ -25,13 +25,27 @@ class MosParams:
 
 
 nch = h.ExternalModule(
-    name="sky130_fd_pr__nfet_01v8", desc="Sky130 NMOS", 
-    port_list=[h.Inout(name="D"), h.Inout(name="G"), h.Inout(name="S"), h.Inout(name="B")], 
-    paramtype=MosParams)
+    name="sky130_fd_pr__nfet_01v8",
+    desc="Sky130 NMOS",
+    port_list=[
+        h.Inout(name="D"),
+        h.Inout(name="G"),
+        h.Inout(name="S"),
+        h.Inout(name="B"),
+    ],
+    paramtype=MosParams,
+)
 pch = h.ExternalModule(
-    name="sky130_fd_pr__pfet_01v8", desc="Sky130 PMOS", 
-    port_list=[h.Inout(name="D"), h.Inout(name="G"), h.Inout(name="S"), h.Inout(name="B")], 
-    paramtype=MosParams)
+    name="sky130_fd_pr__pfet_01v8",
+    desc="Sky130 PMOS",
+    port_list=[
+        h.Inout(name="D"),
+        h.Inout(name="G"),
+        h.Inout(name="S"),
+        h.Inout(name="B"),
+    ],
+    paramtype=MosParams,
+)
 
 
 @h.paramclass
@@ -50,25 +64,27 @@ def common_source_amp_gen(params: CommonSourceParams) -> h.Module:
         vout = h.Output()
         nmos = nch(params.nmos_params)(D=vout, G=vin, S=VSS, B=VSS)
         res = h.Resistor(r=params.res_value)(p=VDD, n=vout)
-    
+
     return CommonSource
 
 
 def get_amp_performance(amp: h.Module) -> Tuple[float, float]:
-    """ Measure the Amplifier Output DC Voltage and Supply Current """
+    """Measure the Amplifier Output DC Voltage and Supply Current"""
     tb = h.sim.tb("CommonSourceTb")
     tb.VDD = h.Signal()
     tb.vout = h.Signal()
     tb.vin = h.Signal()
-    tb.VDD_src = Vdc(Vdc.Params(dc=Decimal('1.8')))(p=tb.VDD, n=tb.VSS)
-    tb.vin_src = Vdc(Vdc.Params(dc='1.5', ac='1.0'))(p=tb.vin, n=tb.VSS)
+    tb.VDD_src = Vdc(Vdc.Params(dc=1800 * m))(p=tb.VDD, n=tb.VSS)
+    tb.vin_src = Vdc(Vdc.Params(dc=1500 * m, ac=1 * UNIT))(p=tb.vin, n=tb.VSS)
     tb.dut = amp(VDD=tb.VDD, VSS=tb.VSS, vin=tb.vin, vout=tb.vout)
 
     sim = h.sim.Sim(tb=tb)
-    sim.lib(f"{CONDA_PREFIX}/share/pdk/sky130A/libs.tech/ngspice/sky130.lib.spice", 'tt')
-    
+    sim.lib(
+        f"{CONDA_PREFIX}/share/pdk/sky130A/libs.tech/ngspice/sky130.lib.spice", "tt"
+    )
+
     sim.op()
-    sim.ac(h.sim.LogSweep(start='1', stop='100e3', npts='10'))
+    sim.ac(h.sim.LogSweep(start="1", stop="100e3", npts="10"))
     sim.literal(".save @m.xtop.xdut.xnmos.msky130_fd_pr__nfet_01v8[vth]")
     sim.literal(".save @m.xtop.xdut.xnmos.msky130_fd_pr__nfet_01v8[gm]")
     sim.literal(".save @m.xtop.xdut.xnmos.msky130_fd_pr__nfet_01v8[id]")
@@ -77,13 +93,14 @@ def get_amp_performance(amp: h.Module) -> Tuple[float, float]:
     results = sim.run(sim_options)
     op_results = results.an[0].data
     ac_results = results.an[1].data
-    vout_dc = op_results['v(xtop.vdd)']
-    dc_current = op_results['i(v.xtop.vvdd_src)']
-    vout_ac = ac_results['v(xtop.vdd)']
+    vout_dc = op_results["v(xtop.vdd)"]
+    dc_current = op_results["i(v.xtop.vvdd_src)"]
+    vout_ac = ac_results["v(xtop.vdd)"]
     breakpoint()
     return vout_dc, dc_current
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     get_amp_performance(
-        common_source_amp_gen(CommonSourceParams(MosParams(w=1, l=0.15, nf=1), 1e3)))
+        common_source_amp_gen(CommonSourceParams(MosParams(w=1, l=0.15, nf=1), 1e3))
+    )
